3eea93ef6f10c380f7a382282199e27ad605f28a
Andrew Stein <ajsforlife@icloud.com>
2022-08-02 11:32:35 -0600

    Add files via upload

--- scraping.py
+++ scraping.py
@@ -15,22 +15,23 @@ def scrape_all():
 
      # Run all scraping functions and store results in a dictionary
      data = {
-         "news_title": news_title,
-         "news_paragraph": news_paragraph,
-         "featured_image": featured_image(browser),
-         "facts": mars_facts(),
-         "last_modified": dt.datetime.now()
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
      }
 
      # Stop webdriver and return data
      browser.quit()
      return data
-     
+
+
 def mars_news(browser):
 
      # Scrape Mars News
      # Visit the mars nasa news site
-     url = 'https://redplanetscience.com/'
+     url = 'https://data-class-mars.s3.amazonaws.com/Mars/index.html'
      browser.visit(url)
 
      # Optional delay for loading the page
@@ -53,9 +54,10 @@ def mars_news(browser):
 
      return news_title, news_p
 
+
 def featured_image(browser):
      # Visit URL
-     url = 'https://spaceimages-mars.com'
+     url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
      browser.visit(url)
 
      # Find and click the full image button
@@ -75,25 +77,25 @@ def featured_image(browser):
          return None
 
      # Use the base url to create an absolute url
-     img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+     img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
 
      return img_url
 
 def mars_facts():
-     # Add try/except for error handling
+    # Add try/except for error handling
      try:
-         # Use 'read_html' to scrape the facts table into a dataframe
-         df = pd.read_html('https://galaxyfacts-mars.com')[0]
+        # Use 'read_html' to scrape the facts table into a dataframe
+         df = pd.read_html('https://data-class-mars-facts.s3.amazonaws.com/Mars_Facts/index.html')[0]
 
      except BaseException:
          return None
 
-     # Assign columns and set index of dataframe
+    # Assign columns and set index of dataframe
      df.columns=['Description', 'Mars', 'Earth']
      df.set_index('Description', inplace=True)
 
      # Convert dataframe into HTML format, add bootstrap
-     return df.to_html()
+     return df.to_html(classes="table table-striped")
 
 if __name__ == "__main__":
 
